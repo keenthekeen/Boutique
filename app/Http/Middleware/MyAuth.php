3664dca3738f44c00efd293bcdc960c9187f157f
piyachetk <piyachetk@gmail.com>
2018-12-31 11:01:22 +0700

    New Auth middleware

---
+++ app/Http/Middleware/MyAuth.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Gate;
+
+class MyAuth {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+        if (!Auth::check()) {
+            return redirect()->guest('/login');
+        }
+        
+        return $next($request);
+    }
+}
