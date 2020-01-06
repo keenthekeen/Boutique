9a4a8b2870e8bc194507b3470a3c6ee9dd7acf2b
iammarkps <markkung08@gmail.com>
2020-01-06 19:12:51 +0700

    Remove more sentry

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,6 @@ class Kernel extends HttpKernel {
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Lock::class,
-            \App\Http\Middleware\SentryContext::class
         ],
         
         'api' => [
